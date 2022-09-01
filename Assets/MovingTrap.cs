d8ff04813807a21009f1f375d8969aa550b94490
Soar TNT <nhutthang.t@gmail.com>
2022-09-02 02:22:05 +0700

    Add trap and moving trap animations to MainScene

---
+++ Assets/MovingTrap.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTrap : MonoBehaviour
+{
+    [SerializeField] private Transform[] movePoints;
+    [SerializeField] private int nextPosition;
+    [SerializeField] private float trapSpeed;
+    [SerializeField] private float rotationMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoints[nextPosition].position, trapSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, movePoints[nextPosition].position) < 0.5f)
+        {
+            nextPosition = Random.Range(0, movePoints.Length);
+            if (nextPosition >= movePoints.Length)
+            {
+                nextPosition = 0;
+            }
+        }
+        if (transform.position.x > movePoints[nextPosition].position.x)
+        {
+            transform.Rotate(new Vector3(0, 0, 100 * rotationMultiplier) * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, 100 * -rotationMultiplier) * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+    }
+}
